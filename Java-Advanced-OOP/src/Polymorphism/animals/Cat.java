4b23c11fb3d9c3bf67a43680e72bd126b84fef1e
Vasilina Lyubomirova <116465286+VLyubomirovaJava@users.noreply.github.com>
2023-03-12 00:59:55 +0200

    Add files via upload

---
+++ Java-Advanced-OOP/src/Polymorphism/animals/Cat.java
@@ -0,0 +1,14 @@
+package animals;
+
+import animals.Animal;
+
+public class Cat extends Animal {
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s MEEOW",this.getName(),this.getFavouriteFood());
+    }
+}
---
+++ Java-Advanced-OOP/src/Polymorphism/animals/Dog.java
@@ -0,0 +1,15 @@
+package animals;
+
+import animals.Animal;
+
+public class Dog extends Animal {
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s DJAAF",this.getName(),this.getFavouriteFood());
+    }
+
+}
